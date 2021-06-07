78621b322877731a2d4c39abb6b1acaf6ca6c5b5
Anna Galsanova <g.anya2001@ya.ru>
2021-06-07 20:19:03 +0800

    app finished

--- app/src/main/java/com/example/projectandroidthirdyear/ProfileActivity.java
+++ app/src/main/java/com/example/projectandroidthirdyear/ProfileActivity.java
@@ -3,12 +3,21 @@ package com.example.projectandroidthirdyear;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import static android.content.ContentValues.TAG;
 
 public class ProfileActivity extends AppCompatActivity {
     TextView account_name, test_title, test_body;
-
+    Button share_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,5 +36,31 @@ public class ProfileActivity extends AppCompatActivity {
                 + "Score: " + getIntent().getStringExtra("scoreINTENT") + " out of 5";
         test_body.setText(result_int);
 
+        share_btn = findViewById(R.id.share_btn);
+
+        String message = title_int + "\n "
+                + result_int;
+
+        share_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://twitter.com/intent/tweet?text=" + urlEncode(message);
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+
+
+
+    }
+
+    private String urlEncode(String s) {
+        try {
+            return URLEncoder.encode(s, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            Log.wtf(TAG, "UTF-8 should always be supported", e);
+            return "";
+        }
     }
 }
